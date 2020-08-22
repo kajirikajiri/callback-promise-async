7d891d775ea881ba7fe7a3526991e3465710af46
kazukinakamuraKN <kajirikajiri@gmail.com>
2020-08-22 18:01:26 +0900

    fix

--- promise.ts
+++ promise.ts
@@ -13,18 +13,28 @@ const promise1 = (b:boolean) =>{
   })
 }
 
+// https://ja.javascript.info/promise-chaining#ref-220
+// thenハンドラは通常、返り値を直ちに次のハンドラにわたします
+// しかし、promiseを受け取った場合にはpromiseの処理を待ちます
+
 promise1(true).then(out => {
   console.log(out)
   console.log('then')
+  // プロミスを返しているわけではないので次の処理に飛ぶ
   setTimeout(()=>{
+    // 四秒後にconsoleが表示される
     console.log(2)
-  }, 2000)
+  }, 4000)
 }).catch(err => {
+  // rejectされないのでcatchに入らない
   console.log(err)
 }).then(()=> {
+  // プロミスなので２秒後に結果が返る
   return promise1(false)
 }).then(ou=>{
+  // rejectされるのでthenに入らない
   console.log(ou)
 }).catch(err=> {
+  // rejectされたのでコンソール
   console.log(err)
 })
